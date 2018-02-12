ac423e35eb0a9daf17b9dfe1bc99a53fe415e545
Sam Davey <samkdavey@gmail.com>
2018-02-12 16:43:50 +0000

    Simple Follow AI added

---
+++ This Is My Stop/Assets/Scripts/BasicAI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAI : MonoBehaviour {
+
+    public Transform target;
+    public Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(target);
+        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+	}
+}

4515cce1804d92e833a474b24d465e0c58820103
Sam Davey <samkdavey@gmail.com>
2018-02-26 02:20:57 +0000

    Punching added & Unity Update Reimport

--- This Is My Stop/Assets/Scripts/PlayerController.cs
+++ This Is My Stop/Assets/Scripts/PlayerController.cs
@@ -15,14 +15,23 @@ public class PlayerController : MonoBehaviour {
     //[SerializeField]
     //private Transform player3;
 
+    //[SerializeField]
+    //private Transform player4;
+
+    private bool punchTrigger1;
+    private bool punchTrigger2;
+
+    [SerializeField]
+    private Collider playerOneFistCollider;
+
     // Use this for initialization
     void Start () {
         anim = GetComponent<Animator>();
+        //playerOneFistCollider = GetComponent<Collider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
         //PLAYER 1 CONTROLS
         var x = Input.GetAxis("Joy1_Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Joy1_Vertical") * Time.deltaTime * -7.0f;
@@ -37,9 +46,46 @@ public class PlayerController : MonoBehaviour {
         player2.Translate(0, 0, b);
 
 
+        //Make into a switch?
+        if(Input.GetKeyDown(KeyCode.Joystick1Button2))
+        {
+            punchTrigger1 = true;
+        }
+        else
+        {
+            punchTrigger1 = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Joystick2Button2))
+        {
+            punchTrigger2 = true;
+        }
+        else
+        {
+            punchTrigger2 = false;
+        }
+
+
         anim.SetFloat("Speed1", z);
         anim.SetFloat("Speed2", b);
-        //Debug.Log("Speed: " + z);
+
+        anim.SetBool("LightPunch", punchTrigger1);
+        anim.SetBool("LightPunch2", punchTrigger2);
+
+        //Debug.Log("Speed is: " + z);
     }
 
+    private void OnCollisionEnter(Collision fistCollision)
+    {
+        if(fistCollision.collider.gameObject == fistCollision.gameObject && fistCollision.gameObject.CompareTag("Team 2"))
+        {
+            //Destroy(fistCollision.collider.gameObject);
+            fistCollision.collider.gameObject.SetActive(false);
+            Debug.Log("Hit Detected!");
+        }
+    }
+    public void PrintEvent(string s)
+    {
+        Debug.Log("PrintEvent: " + s + " called at: " + Time.time);
+    }
 }
